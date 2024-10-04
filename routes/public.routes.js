6f51c24344d854316586cd38c9862b8b110c0eff
priya <priyankatathe4@gmail.com>
2024-10-04 13:31:59 +0530

    equiry email limit

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,14 +1,15 @@
 const router = require("express").Router()
+const { rateLimit } = require("express-rate-limit")
 const public = require('./../controller/public.controller')
 
 router
     .get("/get-carousel", public.getCarousel)
     .get("/get-project", public.getProject)
     .get("/get-project-details/:id", public.getProjectDetails)
-
-    .post("/add-enquiry", public.addEnquiry)
+    //                                    // 15 m 2 email send hoil fkt ashi limit lavta yety 
+    .post("/add-enquiry", rateLimit({ windowMs: 15 * 60 * 1000, limit: 1 }), public.addEnquiry)
     .get("/enquiry", public.getEnquiry)
     .put("/update-enquiry/:id", public.updateEnquiry)
     .delete("/delete-enquiry/:id", public.deleteEnquiry)
 
-module.exports = router
+module.exports = router 
